862e341a603d65ac57e4bde7c30d94846c6ed71c
Maksim Akinshin <otosigen@gmail.com>
2019-10-23 13:33:27 +0300

    key + list element

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,29 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-const todoItem = ['Выбрать ноут', 'Закончить урок'];
-const App = () => (<div>
-    <h1>Важные дела!</h1>
-    <InputItem />
-    <ItemList todoItem={todoItem} />
-    <Footer count={3} />
-</div>);
+const App = () => {
+    const items = [
+        {
+            value: 'Сходить в магазин'
+        },
+        {
+            value: 'Закончить задание'
+        },
+        {
+            value: 'Прочитать статью'
+        },
+        {
+            value: 'Сделать уборку'
+        }
+    ];
+
+    return (
+        <div>
+            <h1>Важные дела!</h1>
+            <InputItem />
+            <ItemList items={items} />
+            <Footer count={4} />
+        </div>);
+}
 
 export default App;
