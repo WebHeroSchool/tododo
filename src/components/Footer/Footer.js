ff6c9ab51cbbb00599504d5aa7cf637fcde69894
Maksim Akinshin <otosigen@gmail.com>
2019-10-25 14:24:49 +0300

    app stylization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import styles from '../Footer/Footer.module.css';
 
-const Footer = ({ count }) => (<span>Осталось выполнить {count}</span>);
-
+const Footer = ({ count }) => (
+    <div className={styles.footer}>
+        <div>
+            <span>Осталось дел: {count}</span>
+        </div>
+        <div className={styles.button__wrap}>
+            <button className={styles.button}>Все</button>
+            <button className={styles.button}>Активные</button>
+            <button className={styles.button}>Завершённые</button>
+            <button className={styles.button}>Удалить выполненные</button>
+        </div>
+    </div >
+);
 export default Footer;
--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import styles from '../InputItem/Input.module.css'
 
 const InputItem = () => (<div>
-    <TextField
+    <TextField className={styles.input}
         id="outlined-dense-multiline"
         label="Добавить задачу"
         margin="dense"
