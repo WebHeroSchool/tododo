96c0155d6bc0766c1f67112872dd533960aa70c9
Maksim Akinshin <otosigen@gmail.com>
2019-10-27 20:13:45 +0300

    add event onclick checkbox

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from '../App/App.module.css';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const Item = ({ value, isDone }) => (<span className={
-    classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-    })
-}>
-    {value}
-</span >);
+const Item = ({ value, isDone, onClickDone }) => (
+    <div className={
+        classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+        })
+    }>
+        <Checkbox
+            color='default'
+            value='checkedI'
+            onClick={() => onClickDone(isDone)}
+        />
+        {value}
+    </div >);
 
 export default Item;
