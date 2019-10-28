40361d95708b8a1eecd98e061f701f253542ef55
Maksim Akinshin <otosigen@gmail.com>
2019-10-28 13:37:16 +0300

    setstate checkbox

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -7,8 +7,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 const ItemList = ({ items, onClickDone }) => (<ul>
     {items.map(item => <li key={item.value} className={styles.item}>
         <div className={styles.task}>
-
-            <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
+            <Item
+                value={item.value}
+                isDone={item.isDone}
+                onClickDone={onClickDone}
+                id={item.id} />
             <button className={styles.button__img}></button>
         </div>
     </li>)}
