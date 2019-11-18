e955f6470732757c0732f0cbca88952318689867
Maksim Akinshin <otosigen@gmail.com>
2019-11-18 14:13:41 +0300

    memory leak

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,15 +6,11 @@ import PropTypes from 'prop-types';
 
 class Item extends React.Component {
     componentDidMount() {
-        console.log('componentDidMount');
-    }
-
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
+        this.timerID = setInterval(() => console.log('componentDidMount'), 1000);
     }
 
     componentWillUnmount() {
-        console.log('componentWillUnmount');
+        clearInterval(this.timerID);
     }
 
     render() {
