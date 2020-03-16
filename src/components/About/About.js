6e94233e7f002771565e81aa05b2b1548d89704e
Maksim Akinshin <otosigen@gmail.com>
2020-03-16 21:13:32 +0300

    about class

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './About.module.css';
 
-const About = () => (
-    <div className={styles.wrap}>
-        <h1 className={styles.title}>Обо мне</h1>
-    </div>
-);
+class About extends React.Component {
+    render() {
+        return (
+            <div className={styles.wrap}>
+                <h1 className={styles.title}>Обо мне</h1>
+            </div>
+        );
+    }
+}
 
 export default About;
