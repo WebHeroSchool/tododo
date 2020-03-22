e67e5f4f3cbc8e2b70d306d0ce794d6d91920fbb
Maksim Akinshin <otosigen@gmail.com>
2020-03-22 16:20:43 +0300

    request fixed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,58 +1,98 @@
-import React from 'react';
-import styles from './About.module.css';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Octokit from '@octokit/rest';
+import React from "react";
+import styles from "./About.module.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Octokit from "@octokit/rest";
+import CardContent from '@material-ui/core/CardContent';
+import Profile from "../Profile/Profile"
 
 const octokit = new Octokit();
 
 class About extends React.Component {
-    state = {
-        isLoading: true,
-        repoList: []
-    }
+  state = {
+    isLoadingRep: true,
+    isLoadingUser: true,
+    isErrorRep: false,
+    isErrorUser: false,
+    errorTextRep: '',
+    errorTextUser: '',
+    repoList: [],
+    user: {}
+  };
 
-    componentDidMount() {
-        octokit.repos.listForUser({
-            username: 'OtoSigen'
-        }).then(({ data }) => {
-            this.setState({
-                repoList: data,
-                isLoading: false
-            });
+  componentDidMount() {
+
+    octokit.repos.listForUser({
+      username: "OtoSigen"
+    })
+      .then(({ data }) => {
+        this.setState({
+          repoList: data,
+          isLoadingRep: false
         });
+      })
+      .catch(() => {
+        this.setState({
+          isLoadingRep: false,
+          isErrorRep: true,
+          errorTextRep: 'Упс, репозитории не найдены!'
+        });
+      });
 
-        octokit.users.getByUsername({
-            username: 'OtoSigen'
-        })
-            .then(({ data }) => {
-                this.setState({
-                    User: data,
-                    isLoadingUser: false,
-                });
-            })
-    }
+    octokit.users.getByUsername({
+      username: "OtoSigen"
+    })
+      .then(({ data }) => {
+        this.setState({
+          user: data,
+          isLoadingUser: false,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          isLoadingUser: false,
+          isErrorUser: true,
+          errorTextUser: 'Пользователь не найден!'
+        });
+      });
+  };
 
-    render() {
-        const { isLoading, repoList, User } = this.state;
-        return (
-            <div className={styles.wrap}>
-                <h1 className={styles.title}>{isLoading ? <CircularProgress /> : 'Обо мне'}</h1>
-                {!isLoading && <ol>
-                    {repoList.map(repo => (<li key={repo.id}>
-                        <a
-                            href={repo.svn_url}
-                            className={styles['info-about-repository-wrapped__link']}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            {repo.name}
-                        </a>
-                    </li>))}
-                </ol>}
-
+  render() {
+    const { isLoadingRep, repoList, isErrorRep, errorTextRep, errorTextUser, user, isLoadingUser, isErrorUser } = this.state;
+    return (
+      <CardContent className={styles.wrap}>
+        <div className={styles.aboutme} >
+          <h1 className={styles.title}>
+            Обо мне
+          </h1>
+          {!isLoadingUser &&
+            <div>
+              {isErrorUser ? errorTextUser :
+                <Profile avatar={user.avatar_url} name={user.name} login={user.login} />
+              }
             </div>
-        );
-    }
+          }
+        </div>
+        <div className={styles.repo} >
+          {isLoadingRep ? <CircularProgress /> : "Список репозиториев:"}
+          {!isLoadingRep &&
+            <div>
+              {isErrorRep ? errorTextRep :
+                <ol>
+                  {repoList.map(repo =>
+                    (<li key={repo.id}>
+                      <a href={repo.svn_url}>
+                        {repo.name}
+                      </a>
+                    </li>
+                    ))}
+                </ol>
+              }
+            </div>
+          }
+        </div>
+      </CardContent >
+    );
+  }
 }
 
 export default About;
