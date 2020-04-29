bbb1eb2c855aab2fec15c1712ff0bf3a2271ec60
Maksim Akinshin <otosigen@gmail.com>
2020-04-29 21:12:51 +0300

    Hooks add in Todo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import styles from '../Footer/Footer.module.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import styles from "../Footer/Footer.module.css";
+import PropTypes from "prop-types";
 
-const Footer = ({ casesCount }) => (
-    <div className={styles.footer}>
-        <div>
-            <span>Осталось дел: {casesCount}</span>
-        </div>
-        <div className={styles.button__wrap}>
-            <button className={styles.button}>Все</button>
-            <button className={styles.button}>Активные</button>
-            <button className={styles.button}>Завершённые</button>
-            <button className={styles.button}>Удалить выполненные</button>
-        </div>
-    </div >
+const Footer = ({ count }) => (
+  <div className={styles.footer}>
+    <div>
+      <span>Осталось дел: {count}</span>
+    </div>
+    <div className={styles.button__wrap}>
+      <button className={styles.button}>Все</button>
+      <button className={styles.button}>Активные</button>
+      <button className={styles.button}>Завершённые</button>
+      <button className={styles.button}>Удалить выполненные</button>
+    </div>
+  </div>
 );
 
 Footer.propTypes = {
-    casesCount: PropTypes.number
+  count: PropTypes.number,
 };
-
-
 
 export default Footer;
